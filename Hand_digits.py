205e456369d259374b0d33673973d89d3b188078
alaporta85 <ndr.lpr@gmail.com>
2018-03-20 17:53:20 +0100

    different optimizers implemented

--- Hand_digits.py
+++ Hand_digits.py
@@ -1,6 +1,7 @@
 import numpy as np
-from sklearn.datasets import load_digits
-from sklearn.utils import shuffle
+from mlxtend.data import loadlocal_mnist
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
@@ -29,27 +30,21 @@ def create_sets(m):
 	   - devel_lab    = same as training_lab. m can be different here.
 	"""
 
-	# Separate images and labels and shuffle them
-	digits = load_digits()
-	images = digits.images
-	labels = digits.target
-	images, labels = shuffle(images, labels, random_state=0)
+	x, y = loadlocal_mnist(
+		images_path='/Users/andrea/Desktop/deeplearning.ai/MNIST_train_set',
+		labels_path='/Users/andrea/Desktop/deeplearning.ai/MNIST_train_labels')
 
-	# Assign the first m samples to training_set and the rest to devel_set
-	training_set = images[:m]                        # Shape: (m, nx, ny)
-	training_lab = from_value_to_onehot(labels[:m])  # Shape: (m, 10)
-	devel_set = images[m:]                           # Shape: (all - m, nx, ny)
-	devel_lab = from_value_to_onehot(labels[m:])     # Shape: (all - m, 10)
+	w, z = loadlocal_mnist(
+		images_path='/Users/andrea/Desktop/deeplearning.ai/MNIST_dev_set',
+		labels_path='/Users/andrea/Desktop/deeplearning.ai/MNIST_dev_labels')
 
-	# Reshape and normalize sets
-	training_set = training_set.reshape(
-			training_set.shape[0], -1).T / 255      # Shape: (nx * ny, m)
-	training_lab = training_lab.reshape(
-			training_lab.shape[0], -1).T            # Shape: (10, m)
-	devel_set = devel_set.reshape(
-			devel_set.shape[0], -1).T / 255         # Shape: (nx * ny, all - m)
-	devel_lab = devel_lab.reshape(
-			devel_lab.shape[0], -1).T               # Shape: (10, all - m)
+	training_set = x[:m, :].T / 255
+	training_lab = from_value_to_onehot(y[:m])
+	training_lab = training_lab.T
+
+	devel_set = w[:m, :].T / 255
+	devel_lab = from_value_to_onehot(z[:m])
+	devel_lab = devel_lab.T
 
 	return training_set, training_lab, devel_set, devel_lab
 
@@ -72,6 +67,7 @@ def from_value_to_onehot(some_array):
 						 [3]                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 
 	"""
+
 	oh = np.zeros((len(some_array), 10))
 	oh[np.arange(len(some_array)), some_array] = 1
 
@@ -101,6 +97,7 @@ def from_array_to_onehot(some_array):
 					[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 					[0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 	"""
+
 	max_index = np.argmax(some_array)
 	oh = np.zeros(some_array.shape)
 	oh[max_index, :] = 1
@@ -119,6 +116,7 @@ def initialize_params(training_set, training_lab, layers,
 
 	   - layers         = list containing the size of each hidden layer (input
 	                      and output layers excluded). Ex: [16, 32].
+
 	   - initialization = float or string.
 
 	   - seed           = bool. If True, seed is used in random initialization.
@@ -147,6 +145,30 @@ def initialize_params(training_set, training_lab, layers,
 		params['b' + str(i)] = np.zeros((layers[i], 1))
 
 	return params
+
+
+def initialize_velocities(grads):
+
+	"""
+	   Initialize with zeros the velocities for RMSprop and Adam optimizers.
+
+	   Inputs:
+	   - grads = dict containing dW and db for each NN's layer.
+
+	   Outputs:
+	   - velocities = dict containing dW and db for each NN's layer.
+	"""
+
+	L = len(grads) // 2
+	velocities = {}
+
+	for i in range(1, L + 1):
+		dw = grads['dW{}'.format(i)]
+		db = grads['db{}'.format(i)]
+		velocities['dW{}'.format(i)] = np.zeros(dw.shape)
+		velocities['db{}'.format(i)] = np.zeros(db.shape)
+
+	return velocities
 
 
 def softmax(z):
@@ -343,6 +365,7 @@ def backward_lin(dz, cache_lin):
 
 	   - cache_lin = tuple
 	"""
+
 	a_previous, w, b = cache_lin
 	m = a_previous.shape[1]
 
@@ -430,7 +453,8 @@ def back_prop(a_final, labels, caches):
 	return grads
 
 
-def update_parameters(params, grads, learning_rate):
+def update_parameters(params, grads, learning_rate, iteration,
+                      beta1, beta2, epsilon, optimizer):
 
 	"""
 	   Update W and b after back propagation.
@@ -442,16 +466,93 @@ def update_parameters(params, grads, learning_rate):
 
 	   - learning_rate = float. Typical value is 0.01
 
+	   - iteration     = int. Running iteration.
+
+	   - beta1         = float. Used in GDM and Adam. Typical value is 0.9
+
+	   - beta2         = float. Used in RMS and Adam. Typical value is 0.999
+
+	   - epsilon       = float. Used in RMS and Adam. Typical value is 1e-8
+
+	   - optimizer     = string. "GD", "GDM", "RMS" or "ADAM".
+
 	   No Outputs.
 	"""
+
 	L = len(params) // 2
 
+	# Calculate bias correction
+	bias_corr1 = (1 - beta1 ** iteration)
+	bias_corr2 = (1 - beta2 ** iteration)
+
+	# Set all gradients values depending on the optimizer
+	if optimizer == 'GD':
+		pass
+
+	elif optimizer == 'GDM':
+		v = initialize_velocities(grads)
+		for i in range(1, L + 1):
+			vdw = v['dW{}'.format(i)]
+			vdb = v['db{}'.format(i)]
+
+			v['dW{}'.format(i)] = (beta1 * vdw + (1 - beta1) *
+			                       grads['dW{}'.format(i)])
+
+			v['db{}'.format(i)] = (beta1 * vdb + (1 - beta1) *
+			                       grads['db{}'.format(i)])
+
+			grads['dW{}'.format(i)] = v['dW{}'.format(i)]
+			grads['db{}'.format(i)] = v['db{}'.format(i)]
+
+	elif optimizer == 'RMS':
+		s = initialize_velocities(grads)
+		for i in range(1, L + 1):
+			sdw = s['dW{}'.format(i)]
+			sdb = s['db{}'.format(i)]
+
+			s['dW{}'.format(i)] = (beta2 * sdw + (1 - beta2) *
+			                       np.square(grads['dW{}'.format(i)]))
+
+			s['db{}'.format(i)] = (beta2 * sdb + (1 - beta2) *
+			                       np.square(grads['db{}'.format(i)]))
+
+			grads['dW{}'.format(i)] /= (np.sqrt(s['dW{}'.format(i)]) + epsilon)
+			grads['db{}'.format(i)] /= (np.sqrt(s['db{}'.format(i)]) + epsilon)
+
+	elif optimizer == 'ADAM':
+		v = initialize_velocities(grads)
+		s = initialize_velocities(grads)
+		for i in range(1, L + 1):
+			vdw = v['dW{}'.format(i)]
+			vdb = v['db{}'.format(i)]
+			sdw = s['dW{}'.format(i)]
+			sdb = s['db{}'.format(i)]
+
+			v['dW{}'.format(i)] = (beta1 * vdw + (1 - beta1) *
+			                       grads['dW{}'.format(i)])
+			v['db{}'.format(i)] = (beta1 * vdb + (1 - beta1) *
+			                       grads['db{}'.format(i)])
+			s['dW{}'.format(i)] = (beta2 * sdw + (1 - beta2) *
+			                       np.square(grads['dW{}'.format(i)]))
+			s['db{}'.format(i)] = (beta2 * sdb + (1 - beta2) *
+			                       np.square(grads['db{}'.format(i)]))
+
+			vdw_corr = v['dW{}'.format(i)] / bias_corr1
+			vdb_corr = v['db{}'.format(i)] / bias_corr1
+			sdw_corr = s['dW{}'.format(i)] / bias_corr2
+			sdb_corr = s['db{}'.format(i)] / bias_corr2
+
+			grads['dW{}'.format(i)] = vdw_corr / (np.sqrt(sdw_corr) + epsilon)
+			grads['db{}'.format(i)] = vdb_corr / (np.sqrt(sdb_corr) + epsilon)
+
+	# Update W and b
 	for i in range(1, L + 1):
 		params['W{}'.format(i)] -= learning_rate * grads['dW{}'.format(i)].T
 		params['b{}'.format(i)] -= learning_rate * grads['db{}'.format(i)]
 
 
-def forw_back_upd(a0, labels, params, learning_rate):
+def forw_back_upd(a0, labels, params, learning_rate,
+                  iteration, beta1, beta2, epsilon, optimizer):
 
 	"""
 	   Compute forward prop, cost, back prop and update W and b.
@@ -465,6 +566,16 @@ def forw_back_upd(a0, labels, params, learning_rate):
 
 	   - learning_rate = float.
 
+	   - iteration     = int. Running iteration.
+
+	   - beta1         = float. Used in GDM and Adam. Typical value is 0.9
+
+	   - beta2         = float. Used in RMS and Adam. Typical value is 0.999
+
+	   - epsilon       = float. Used in RMS and Adam. Typical value is 1e-8
+
+	   - optimizer     = string. "GD", "GDM", "RMS" or "ADAM".
+
 	   Outputs:
 	   - cost = float.
 	"""
@@ -472,23 +583,27 @@ def forw_back_upd(a0, labels, params, learning_rate):
 	a_final, cache = forward_prop(a0, params)
 	cost = compute_cost(a_final, labels)
 	grads = back_prop(a_final, labels, cache)
-	update_parameters(params, grads, learning_rate)
+	update_parameters(params, grads, learning_rate,
+	                  iteration, beta1, beta2, epsilon, optimizer)
 
 	return cost
 
 
-def compute_accuracy(a_final, labels, which_set):
+def compute_accuracy(a_final, labels, which_set, print_accuracy):
 
 	"""
 	   Compute accuracy of the model.
 
 	   Inputs:
-	   - a_final = numpy array.
+	   - a_final        = numpy array.
 
-	   - labels  = numpy array.
+	   - labels         = numpy array.
 
-	   Outputs:
-	   - accuracy = float.
+	   - which_set      = string.
+
+	   - print_accuracy = bool.
+
+	   No Outputs.
 	"""
 
 	nx = a_final.shape[0]
@@ -504,70 +619,185 @@ def compute_accuracy(a_final, labels, which_set):
 
 	accuracy = round(count / m * 100, 1)
 
-	print('Accuracy for {} set: {} %'.format(which_set, accuracy))
+	if print_accuracy:
+		print('Accuracy for {} set: {} %'.format(which_set, accuracy))
 
 
 def model_plain(input_set, input_lab, params, learning_rate,
-                iterations, plot_cost=False):
+                learning_rate_decay, iterations, beta1, beta2, epsilon,
+                optimizer, print_every, plot_cost=False, print_accuracy=False):
+	"""
+	   Train the model.
+
+	   Inputs:
+	   - input_set           = numpy array. The input set of images.
+
+	   - input_lab           = numpy array.
+
+	   - params              = dict containing W and b.
+
+	   - learning_rate       = float.
+
+	   - learning_rate_decay = float.
+
+	   - iterations          = int.
+
+	   - beta1               = float. Used in GDM/Adam. Typical value is 0.9
+
+	   - beta2               = float. Used in RMS/Adam. Typical value is 0.999
+
+	   - epsilon             = float. Used in RMS/Adam. Typical value is 1e-8
+
+	   - optimizer           = string. "GD", "GDM", "RMS" or "ADAM".
+
+	   - print_every         = int. Number of iterations to print the cost.
+	                           If == "last", only last cost will be printed.
+
+	   - plot_cost           = bool.
+
+	   - print_accuracy      = bool.
+
+	   Outputs:
+	   - all_cost = list.
+	"""
 
 	all_costs = []
 
 	for i in range(iterations + 1):
-		cost = forw_back_upd(input_set, input_lab, params, learning_rate)
+		alpha = (1 / (1 + learning_rate_decay * i)) * learning_rate
+		cost = forw_back_upd(input_set, input_lab, params, alpha,
+		                     i + 1, beta1, beta2, epsilon, optimizer)
 		all_costs.append(cost)
 
-		if i % 5000 == 0:
+		if type(print_every) == int and i % print_every == 0:
 			print("Cost at iteration {}: {}".format(i, round(cost, 4)))
 
+	if print_every == 'last':
+		print("Cost at last epoch for {} optimizer: {}".format(
+				optimizer, round(cost, 4)))
+
 	a_final, _ = forward_prop(input_set, params)
-	compute_accuracy(a_final, input_lab, 'training')
+	compute_accuracy(a_final, input_lab, 'training', print_accuracy)
 
 	if plot_cost:
-		fig = plt.plot(all_costs)
+		plt.plot(all_costs, label='Plain, {}'.format(optimizer))
+		plt.legend()
 		plt.show()
+
+	return all_costs
 
 
 def model_mini_batch(input_set, input_lab, params, learning_rate,
-                     epochs, batch_size, plot_cost=False):
+                     learning_rate_decay, epochs, batch_size,
+                     beta1, beta2, epsilon, optimizer, print_every,
+                     plot_cost=False, print_accuracy=False):
+	"""
+	   Train the model.
 
-	all_costs = []
+	   Inputs:
+	   - input_set           = numpy array. The input set of images.
+
+	   - input_lab           = numpy array.
+
+	   - params              = dict containing W and b.
+
+	   - learning_rate       = float.
+
+	   - learning_rate_decay = float.
+
+	   - epochs              = int.
+
+	   - batch_size          = int.
+
+	   - beta1               = float. Used in GDM/Adam. Typical value is 0.9
+
+	   - beta2               = float. Used in RMS/Adam. Typical value is 0.999
+
+	   - epsilon             = float. Used in RMS/Adam. Typical value is 1e-8
+
+	   - optimizer           = string. "GD", "GDM", "RMS" or "ADAM".
+
+	   - print_every         = int. Number of iterations to print the cost.
+	                           If == "last", only last cost will be printed.
+
+	   - plot_cost           = bool.
+
+	   - print_accuracy      = bool.
+
+	   Outputs:
+	   - all_cost = list.
+	"""
+
+	cost_epochs = []
+	cost_batches = []
 	mini_batches = input_set.shape[1] // batch_size + 1 if \
 		input_set.shape[1] % batch_size else input_set.shape[1] // batch_size
 
 	for i in range(epochs + 1):
+		alpha = (1 / (1 + learning_rate_decay * i)) * learning_rate
 		cost = 0
 		for x in range(mini_batches):
 			inp = input_set[:, x * batch_size: (x + 1) * batch_size]
 			label = input_lab[:, x * batch_size: (x + 1) * batch_size]
-			cost += forw_back_upd(inp, label, params, learning_rate)
+			cost += forw_back_upd(inp, label, params, alpha,
+			                      i + 1, beta1, beta2, epsilon, optimizer)
+			cost_batches.append(cost)
 		cost /= mini_batches
-		all_costs.append(cost)
+		cost_epochs.append(cost)
 
-		if i % 2000 == 0:
+		if type(print_every) == int and i % print_every == 0:
 			print("Cost at epoch {}: {}".format(i, round(cost, 4)))
 
-	a_final, _ = forward_prop(input_set, params)
-	compute_accuracy(a_final, input_lab, 'training')
+	if print_every == 'last':
+		print("Cost at last epoch for {} optimizer: {}".format(
+				optimizer, round(cost, 4)))
 
-	if plot_cost:
-		fig = plt.plot(all_costs)
+	a_final, _ = forward_prop(input_set, params)
+	compute_accuracy(a_final, input_lab, 'training', print_accuracy)
+
+	if plot_cost == 'each_epoch':
+		plt.plot(cost_epochs, label='MB, {}'.format(optimizer))
+		plt.legend()
 		plt.show()
+	elif plot_cost == 'each_mini_batch':
+		plt.plot(cost_batches, label='MB, {}'.format(optimizer))
+		plt.legend()
+		plt.show()
+
+	return cost_batches, cost_epochs
 
 
 def test_dev_set(devel_set, devel_lab, params):
+
+	"""
+	   Test the model on dev set.
+
+	   Inputs:
+	   - devel_set = numpy array.
+
+	   - devel_lab = numpy array.
+
+	   - params    = dict. Trained parameters.
+
+	   No Outputs.
+	"""
+
 	a_final, _ = forward_prop(devel_set, params)
-	compute_accuracy(a_final, devel_lab, 'dev')
+	compute_accuracy(a_final, devel_lab, 'dev', True)
 
 
-hidden_layers = [16, 16, 16, 16]
-train_set, train_lab, dev_set, dev_lab = create_sets(m=1500)
-parameters = initialize_params(train_set, train_lab, hidden_layers,
-                               initialization='xavier', seed=False)
+# hidden_layers = [32, 32, 32]
+# train_set, train_lab, dev_set, dev_lab = create_sets(m=60000)
+# parameters = initialize_params(train_set, train_lab, hidden_layers,
+#                                initialization='xavier', seed=True)
 
 # model_plain(train_set, train_lab, parameters, learning_rate=0.01,
-#             iterations=1000, plot_cost=False)
+#             iterations=2, plot_cost=False)
 
 
-model_mini_batch(train_set, train_lab, parameters, learning_rate=0.01,
-                 epochs=20000, batch_size=256, plot_cost=False)
+# cost_batches_GD, cost_epochs_GD = model_mini_batch(
+# 		train_set, train_lab, parameters, learning_rate=0.00001,
+# 		learning_rate_decay=0.005, epochs=1, batch_size=1024,
+# 		beta1=0.9, beta2=0.999, epsilon=1e-8, optimizer='RMS',
+# 		print_every='last', plot_cost=False, print_accuracy=False)
 # test_dev_set(dev_set, dev_lab, parameters)
